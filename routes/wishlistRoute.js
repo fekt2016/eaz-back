e058bd12b0f4c1e034b48944b801edd244d0734b
yussif faisal <fekt2016@icloud.com>
2025-08-31 22:25:28 +0000

    testing deployment

--- routes/wishlistRoute.js
+++ routes/wishlistRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const wishlistController = require('../controllers/wishlistController');
+const wishlistController = require('../Controllers/wishlistController');
 const authController = require('../Controllers/authController');
 
 // // Public routes (for guest users)
